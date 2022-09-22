32a05f27b55812167ab9df41e72bf15b5c481a3f
Nathaly Toledo <nathaly12toledo@gmail.com>
2022-09-22 16:16:55 -0400

    feat: create React routes
    Created React routes and base pages for each relevant view corresponding to a wireframe.

---
+++ frontend/src/pages/contributions_in_progress_feed.js
@@ -0,0 +1,9 @@
+import React from 'react';
+  
+const ContributionsFeedInProgress = () => {
+  return (
+    <h1>Contributions Feed In Progress page</h1>
+  );
+};
+  
+export default ContributionsFeedInProgress;

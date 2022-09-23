14e68dff1599688731925fb7dfbbccff6c065a4f
Nathaly Toledo <nathaly12toledo@gmail.com>
2022-09-23 03:01:45 -0400

    feat: Create UI for the available open source projects feed
    Added base component to list each element.

--- frontend/src/pages/contributions_feed.js
+++ frontend/src/pages/contributions_feed.js
@@ -1,9 +1,55 @@
 import React from 'react';
-  
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import CardActions from '@mui/material/CardActions'
+
+
 const ContributionsFeed = () => {
   return (
-    <h1>Contributions Feed page!</h1>
+
+    <div style={{padding: 20, margin: "auto", maxWidth: "1000px"}}>
+      <h1>Contributions Feed page!</h1>
+      <Card
+        style={{
+          width: 400,
+          backgroundColor: "yellow",
+          minWidth: "1000px",
+          padding: 20
+        }}
+      >
+        <CardContent>
+          <Typography style={{ fontSize: 14 }} color="textSecondary" gutterBottom>
+            Available open source project
+          </Typography>
+
+          <Typography variant="h5" component="h2">
+            How are you ?
+          </Typography>
+
+          <Typography
+            style={{
+              marginBottom: 12,
+            }}
+            color="textSecondary"
+          >
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
+            molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
+            numquam blanditiis.
+          </Typography>
+
+
+        </CardContent>
+
+        <CardActions style={{textAlign: "right"}}>
+          <Button size="medium" style={{backgroundColor: "white", color: "black" }}>Contribute to project</Button>
+        </CardActions>
+
+      </Card>
+    </div>
+
   );
 };
-  
+
 export default ContributionsFeed;
